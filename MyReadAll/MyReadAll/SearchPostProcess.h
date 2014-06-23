bc629eb534de2f286f6114680a0013bcabb5c1eb
phillipcheng <phillipchengyi@gmail.com>
2014-06-23 04:49:10 -0700

    add

--- MyReadAll/MyReadAll/SearchPostProcess.h
+++ MyReadAll/MyReadAll/SearchPostProcess.h
@@ -6,8 +6,10 @@
 //  Copyright (c) 2014 Cheng Yi. All rights reserved.
 //
 
+#import "SearchResult.h"
+
 @protocol SearchPostProcess <NSObject>
 
--(void) postProcess:(NSString*) searchTxt searchCat:(NSString*) catId offset:(int) offset limit:(int) limit result:(NSArray*) result;
+-(void) postProcess:(NSString*) searchTxt searchCat:(NSString*) catId offset:(int) offset limit:(int) limit result:(SearchResult*) result;
 
 @end
