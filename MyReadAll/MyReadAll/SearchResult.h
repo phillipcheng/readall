3d0f1f81ccb1b5f84e15c75fb272524c65b47a12
phillipcheng <phillipchengyi@gmail.com>
2014-06-23 04:49:56 -0700

    add

---
+++ MyReadAll/MyReadAll/SearchResult.h
@@ -0,0 +1,14 @@
+//
+//  SearchResult.h
+//  MyReadAll
+//
+//  Created by Cheng Yi on 6/22/14.
+//  Copyright (c) 2014 Cheng Yi. All rights reserved.
+//
+
+@interface SearchResult: NSObject
+
+@property (nonatomic) NSArray* readings;
+@property (nonatomic) int count; //count * of the search condition
+
+@end
---
+++ MyReadAll/MyReadAll/SearchResult.m
@@ -0,0 +1,15 @@
+//
+//  SearchResult.m
+//  MyReadAll
+//
+//  Created by Cheng Yi on 6/22/14.
+//  Copyright (c) 2014 Cheng Yi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SearchResult.h"
+
+@implementation SearchResult 
+
+
+@end
