7fd2832ba8d5db66b0905ab1ae4d7d8835dd338c
phillipcheng <phillipchengyi@gmail.com>
2014-06-23 04:48:35 -0700

    add

---
+++ MyReadAll/MyReadAll/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  MyReadAll
+//
+//  Created by Cheng Yi on 6/14/14.
+//  Copyright (c) 2014 Cheng Yi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
---
+++ MyReadAll/MyReadAll/SecondViewController.m
@@ -0,0 +1,28 @@
+//
+//  SecondViewController.m
+//  MyReadAll
+//
+//  Created by Cheng Yi on 6/14/14.
+//  Copyright (c) 2014 Cheng Yi. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+            
+
+@end
+
+@implementation SecondViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
